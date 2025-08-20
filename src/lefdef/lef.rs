ff78c513a67038e7ee76c5ab410c7762968e1ec6
yongqi <yongqi@icbench.com>
2025-08-20 17:40:24 +0800

    run with some known bugs

--- src/lefdef/lef.rs
+++ src/lefdef/lef.rs
@@ -1,14 +1,16 @@
-use std::{io::Write, path::PathBuf};
+use std::{io::Write, os::unix::ffi::OsStringExt, path::PathBuf};
 
 use crate::{lefdef::writer::Macros, parser::Bookshelf};
 
 pub struct Lef {
     pub macros: Macros,
+    pub site_height: f64,
 }
 
 impl Lef {
     pub async fn build(bookshelf: &Bookshelf) -> anyhow::Result<Self> {
         Ok(Self {
+            site_height: bookshelf.scl.iter().next().unwrap().height as f64,
             macros: Macros::build_macro(bookshelf).await?,
         })
     }
@@ -22,9 +24,9 @@ DIVIDERCHAR "/" ;
 SITE CoreSite 
     CLASS CORE ;
     SYMMETRY Y ;
-    SIZE 1.000 BY 9.000 ;
+    SIZE 1.000 BY {:.3} ;
 END CoreSite
-"#);
+"#, self.site_height);
         to_write += &self.macros.write_all();
         let mut f = std::fs::File::create(file_path)?;
         f.write_all(to_write.as_bytes())?;
